f035e1f2e3e234c78b882f9df5d9eda687b57e36
Legion <emansobhey8@gmail.com>
2024-09-15 09:31:28 +0300

    add Splash Screen in night and light

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/screens/Home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+        home: HomeScreen()
     );
   }
 }
